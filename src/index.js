aa089053e088a41b7b7d06a2a6508ee7abf9291e
setfocusbrasil <149403481+conectfam@users.noreply.github.com>
2023-11-13 12:18:22 -0300

    tag painel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import * as gtag from "../lib/gtag";
+
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
